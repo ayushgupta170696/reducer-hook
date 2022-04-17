16d506d12627716f13167a08583316e27272e30f
ayushgupta170696 <103874355+ayushgupta170696@users.noreply.github.com>
2022-04-17 20:48:15 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import './App.css';
+import CounterOne from './components/CounterOne';
+import CounterTwo from './components/CounterTwo';
+import CounterThree from './components/CounterThree';
+import DataFetching from './components/DataFetching';
+import DataFetchingTwo from './components/DataFetchingTwo';
+
+function App() {
+  return (
+    <div className="App">
+     {/* <CounterOne></CounterOne> */}
+     <DataFetchingTwo></DataFetchingTwo>
+    </div>
+  );
+}
+
+export default App;
